c5220c75b8ce85ed751ab03fb29d71bb3b125d8a
J4nni <jan-niklas.moebus@t-online.de>
2023-01-13 21:36:07 +0100

    Add requirements txt and renaming Variables

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
 # Currentify
-Add the current playing Song in Spotify to your playlist over Python :)
+Add the current playing Song in Spotify to your playlist with Python :)
 
 # Requirements
-* Python Modules: https://github.com/plamere/spotipy
+* pip freeze -r requirements.txt
 * Create your own APP on Spotify
 
 # Installation
 1. Install PIP&Python
-2. Install Spotipy `pip install Spotipy`
-3. Edit the Spotify.py at Line 11 and 12 with your Client ID and your Secret.
-4. Open a Bash/Shell/Terminal
-5. In the Bash, go to your Clone Directory and type `python Spotify.py`
-6. Or Maybe This way ![](Gif.gif)
+2. Edit the Spotify.py at Line 11 and 12 with your Client ID and your Secret.
+3. Open a Bash/Shell/Terminal
+4. In the Bash, go to your Clone Directory and type `python Spotify.py`
+5. Or Maybe This way ![](Gif.gif)
 
 
 # Notes
--- Spotify.py
+++ Spotify.py
@@ -6,7 +6,7 @@ import requests
 req=requests
 
 token = util.prompt_for_user_token(
-        username='$your username',
+        username='$yourusername',
         scope='user-read-private user-read-currently-playing user-library-modify playlist-read-collaborative playlist-modify-public playlist-modify-private',
         client_id='XXX', #Add your Client ID
         client_secret='XXX', #Add your Client Secret
@@ -14,18 +14,18 @@ token = util.prompt_for_user_token(
 
 spotify = spotipy.Spotify(auth=token)
 hed = {'Authorization': 'Bearer ' + token}
-insa = req.get(url="https://api.spotify.com/v1/me/player/currently-playing", headers=hed) #Get your Current Song
-play=req.get(url="https://api.spotify.com/v1/me/playlists", headers=hed) #Get your Playlists
+currentsong = req.get(url="https://api.spotify.com/v1/me/player/currently-playing", headers=hed) #Get your Current Song
+playlist=req.get(url="https://api.spotify.com/v1/me/playlists", headers=hed) #Get your Playlists
+playlist.content
 
-
-rawjson= json.loads(insa.content) #Check the Currentsong 
-if rawjson['is_playing'] is False:
+rawjson= json.loads(currentsong.content) #loads the content from the currentsong in json 
+if rawjson['is_playing'] is False: 
     exit(print("Hey you don't listen any Music in Spotify?!"))
 else:
     itemkey= rawjson ['item']
     TrackUrl=itemkey['id']
     ActuallySong=[TrackUrl] #The Endpoint needs a List to add the Song 
-    playlistjs=play.content
+    playlistjs=playlist.content
     playlists= json.loads(playlistjs)
     items=playlists['items']
     increment=0
@@ -38,11 +38,11 @@ else:
         collab=s['collaborative']
         owners=s['owner']
         displayname=owners['display_name']
-        if collab is False and displayname != '$yourusername' : #catch Protected Playlist
+        if collab is False and displayname != '$yourusername' : #catch Protected Playlist where only read
             newbuild= str(increment) +" "+ n
             Titel+=[newbuild]
             playid+=[d]
-            print (str(increment)+" "+"no Permission for Playlist "+n +" to add songs")
+            #print (str(increment)+" "+"no Permission for Playlist "+n +" to add songs")
             increment = increment+1
         else: #here we build the informations
             newbuild= str(increment) +" "+ n 
@@ -63,4 +63,4 @@ while f is True:
 try:
    spotify.user_playlist_add_tracks('$username',playid[nummer],ActuallySong)
 except spotipy.client.SpotifyException as identifier:
- print("No Permission")
+ print("No Permission ")
